da1f8db74827045370ae8ebd820ab929e2ea7970
Zixiang XUN <psxzx4@nottingham.ac.uk>
2020-02-06 16:44:34 +0000

    first commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(1, 0, 0), ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
